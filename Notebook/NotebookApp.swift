5a0112da095dfe68efb182d5677d209c6e09ed9e
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2025-03-11 17:21:08 +0500

    Initial Commit

---
+++ Notebook/NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotebookApp.swift
+//  Notebook
+//
+//  Created by Benji Loya on 11.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
